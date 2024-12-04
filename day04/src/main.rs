5e00716013cb6abaac87eeb5906afba94c583118
H4rdeol <leonard.oursel@epitech.eu>
2024-12-04 15:19:30 +0100

    [+] day04 step 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -29,10 +29,21 @@ fn line_find(array: &Vec<String>, mut i: i32, mut j: i32, direction: (i32, i32),
     return if index == word.len() {1} else {0}
 }
 
-fn main() {
-    let array: Vec<String> = get_file();
-    let mut count = 0;
+fn get_matrix3x3(array: &Vec<String>, i: i32, j: i32) -> Option<Vec<String>> {
+    let mut matrix: Vec<String> = Vec::new();
 
+    for x in -1..2 {
+        if i + x >= 0 && j - 1 >= 0 && i + x < array.len() as i32 && j + 1 < array[0].len() as i32 {
+            matrix.push(array[(i + x) as usize][(j - 1) as usize..(j + 2) as usize].to_string());
+        } else {
+            return None;
+        }
+    }
+    Some(matrix)
+}
+
+fn step_1(array: &Vec<String>) -> i32 {
+    let mut count = 0;
     for i in 0..(array.len() as i32) {
         for j in 0..(array[i as usize].len() as i32) {
             count += line_find(&array, i, j, (0, 1), "XMAS") +
@@ -45,5 +56,33 @@ fn main() {
             line_find(&array, i, j, (1, -1), "XMAS");
         }
     }
-    println!("step-1: {:?}", count);
+    count
+}
+
+fn step_2(array: &Vec<String>) -> i32 {
+    let mut res = 0;
+
+    for i in 0..(array.len() as i32) {
+        for j in 0..(array[i as usize].len() as i32) {
+            if array[i as usize].chars().nth(j as usize).unwrap() == 'A' {
+                if let Some(a) =  get_matrix3x3(array, i, j) {
+                    let count = line_find(&a, 0, 0, (1, 1), "MAS") +
+                                line_find(&a, 2, 0, (-1, 1), "MAS") +
+                                line_find(&a, 0, 2, (1, -1), "MAS") + 
+                                line_find(&a, 2, 2, (-1, -1), "MAS");
+                    if count >= 2 {
+                        res += 1;
+                    }
+                } 
+            }
+        }
+    }
+    res
+}
+
+fn main() {
+    let array: Vec<String> = get_file();
+    
+    println!("step-1: {:?}", step_1(&array));
+    println!("step-2: {:?}", step_2(&array));
 }
