b66073bcbe605b8665f2f5869e7e6e8df8924f64
H4rdeol <leonard.oursel@epitech.eu>
2024-12-09 10:55:51 +0100

    [+] step 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -30,34 +30,59 @@ fn create_first_tab(array: &mut Iter<'_, String>) -> Result<Vec<(i32, i32)>, Err
     Ok(orders)
 }
 
-fn verify_rule(line_int: &Vec<i32>, orders: &Vec<(i32, i32)>) -> bool {
+fn verify_rule(line_int: &Vec<i32>, rule: Vec<i32>, i: usize) -> Option<usize> {
+    for j in 0..i {
+        if rule.contains(line_int.get(j).unwrap()) {
+            return Some(j);
+        }
+    }
+    None
+}
+
+fn verify_rules(line_int: &Vec<i32>, orders: &Vec<(i32, i32)>) -> bool {
 
     for i in 1..line_int.len() {
         let rules: Vec<i32> = orders.iter().filter(|(a, _)| a == line_int.get(i).unwrap()).map(|(_, b)| *b).collect();
-        for j in 0..i {
-            if rules.contains(line_int.get(j).unwrap()) {
-                return false;
-            }
+        if let Some(_) = verify_rule(line_int, rules, i) {
+            return false;
         }
     }
     true
+}
+
+fn correct_orders(line_int: &mut Vec<i32>, orders: &Vec<(i32, i32)>) -> i32 {
+    let mut i = 1;
+    while i < line_int.len() {
+        let rules: Vec<i32> = orders.iter().filter(|(a, _)| a == line_int.get(i).unwrap()).map(|(_, b)| *b).collect();
+        if let Some(broken_rule) = verify_rule(line_int, rules.clone(), i) {
+            line_int.swap(i, broken_rule);
+            i = 1;
+        } else {
+            i += 1;
+        }
+    }
+    *line_int.get(line_int.len() / 2).unwrap()
 }
 
 fn main() {
     let array = get_file();
     let mut iter = array.iter();
     let mut page_numbers_step1: Vec<i32> = Vec::new();
+    let mut page_numbers_step2: Vec<i32> = Vec::new();
     
     if let Ok(orders) = create_first_tab(&mut iter) {
         while let Some(line) = iter.next() {
             let line_int: Vec<i32> = line.split(',').collect::<Vec<&str>>().iter()
             .filter_map(|x| x.parse::<i32>().ok())
             .collect();
-            if verify_rule(&line_int, &orders) {
+            if verify_rules(&line_int, &orders) {
                 page_numbers_step1.push(*line_int.get(line_int.len() / 2).unwrap());
+            } else {
+                page_numbers_step2.push(correct_orders(&mut line_int.clone(), &orders));
             }
         }
         println!("step-1: {:?}", page_numbers_step1.iter().sum::<i32>());
+        println!("step-2: {:?}", page_numbers_step2.iter().sum::<i32>());
     } else {
         println!("Error");
     }
